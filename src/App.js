f208e3b34e6cd04cb2e3662cc4188016d1e3ba1a
anastasiiiii <nastia262002@gmail.com>
2024-11-02 13:24:05 +0100

    add text containers that are near the image containers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import bgImage from "./images/desktop/image-transform.jpg";
 import bgImage2 from "./images/desktop/image-stand-out.jpg";
 import bgImage3 from "./images/desktop/image-graphic-design.jpg";
 import bgImage4 from "./images/desktop/image-photography.jpg";
+import TextBox from './components/TextBox';
 
 
 
@@ -13,7 +14,7 @@ function App() {
   //const bgImage = "./images/desktop/image-transform.jpg";
 
   const header1 = "Graphic Design";
-  const passage1 ="Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients attention.";
+  const passage1 = "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients attention.";
 
   const header2 = "Photography";
   const passage2 = "Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
@@ -24,11 +25,19 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Image className="right-image" bgImage={bgImage} />
-      <Image bgImage={bgImage2} />
+      <span className='first-two-images'>
+        <TextBox header={header1} passage={passage1} />
+        <Image className="right-image" bgImage={bgImage} />
+      </span>
+      <span className='second-two-images'>
+        <Image bgImage={bgImage2} />
+        <TextBox header={header1} passage={passage1} />
+      </span>
+
+
       <span className='down-two-images'>
         <Image bgImage={bgImage3} header={header1} text={passage1} textColor={textColor1} />
-        <Image bgImage={bgImage4} header={header2} text={passage2} textColor={textColor2} /> 
+        <Image bgImage={bgImage4} header={header2} text={passage2} textColor={textColor2} />
       </span>
     </div>
   );
---
+++ src/components/TextBox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/TextBox.css";
+
+const TextBox = ({ title, passage }) => {
+    return (
+        <div className="text-side-container">
+            <h1>{title}</h1>
+            <p id="passage">{passage}</p>
+            <p id="learn-more-button">Learn more</p>
+        </div>
+    )
+}
+
+export default TextBox;
