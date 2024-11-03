22ccfbb485d7e3e36897352b00af7908fd771460
anastasiiiii <nastia262002@gmail.com>
2024-11-03 17:16:33 +0100

    add responsive adaptive header and main container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import './styles/Formulas.css';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Image from './components/Image';
 import ClientCase from './components/ClientCase';
 import TextBox from './components/TextBox';
 import ImgLine from './components/ImgLine';
 import Footer from './components/Footer';
-import bgImage from "./images/desktop/image-transform.jpg";
-import bgImage2 from "./images/desktop/image-stand-out.jpg";
-import bgImage3 from "./images/desktop/image-graphic-design.jpg";
-import bgImage4 from "./images/desktop/image-photography.jpg";
+import bgImageBig from "./images/desktop/image-transform.jpg";
+import bgImageSmall from "./images/mobile/image-transform.jpg";
+import bgImage2Big from "./images/desktop/image-stand-out.jpg";
+import bgImage2Small from "./images/mobile/image-stand-out.jpg";
+import bgImage3Big from "./images/desktop/image-graphic-design.jpg";
+import bgImage4Big from "./images/desktop/image-photography.jpg";
 
 function App() {
   //const bgImage = "./images/desktop/image-transform.jpg";
+  const [isMobile, setIsMobile] = useState(typeof window !== "undefined" && window.innerWidth < 500);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 500);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const header1 = "Graphic Design";
   const passage1 = "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients attention.";
@@ -32,21 +48,29 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <span className='first-two-images'>
-        <TextBox title={header3} passage={passage3} />
-        <Image className="right-image" bgImage={bgImage} />
-      </span>
+      {isMobile ? (
+        <span className='first-two-images'>
+          <Image className="right-image" bgImage={bgImageSmall} />
+          <TextBox title={header3} passage={passage3} />
+        </span>
+      ) : (
+        <span className='first-two-images'>
+          <TextBox title={header3} passage={passage3} />
+          <Image className="right-image" bgImage={bgImageBig} />
+        </span>
+      )}
+
       <span className='second-two-images'>
-        <Image bgImage={bgImage2} />
+        <Image bgImage={isMobile ? bgImage2Small : bgImage2Big} />
         <TextBox title={header4} passage={passage4} />
       </span>
       <span className='down-two-images'>
-        <Image bgImage={bgImage3} header={header1} text={passage1} textColor={textColor1} />
-        <Image bgImage={bgImage4} header={header2} text={passage2} textColor={textColor2} />
+        <Image bgImage={bgImage3Big} header={header1} text={passage1} textColor={textColor1} />
+        <Image bgImage={bgImage4Big} header={header2} text={passage2} textColor={textColor2} />
       </span>
-      <ClientCase />
+      {/* <ClientCase />
       <ImgLine />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Header.css";
 import Menu from "./Menu";
+import bgImageBig from "../images/desktop/image-header.jpg";
+import bgImageSmall from "../images/mobile/image-header.jpg";
 
 const Header = () => {
+    const [isMobile, setIsMobile] = useState(typeof window !== "undefined" && window.innerWidth < 500);
+
     const arrowDownIcon = (
         <svg width="36" height="114" xmlns="http://www.w3.org/2000/svg">
             <g stroke="#FFF" strokeWidth="6" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
@@ -11,9 +15,21 @@ const Header = () => {
         </svg>
     );
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 500);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
-        <div className="header-container">
-            <Menu />
+        <div className="header-container" style={{ backgroundImage: `url(${isMobile ? bgImageBig : bgImageBig})` }}>
+            <Menu isMobile={isMobile} />
             <div className="header-main-text">
                 <p>We are creatives</p>
                 <span id="arrow-down-icon">{arrowDownIcon}</span>
